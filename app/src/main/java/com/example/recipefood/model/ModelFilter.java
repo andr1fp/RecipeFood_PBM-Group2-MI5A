0e4892b943ce9440e27baacb49015bf09dc59587
AdindaEliDahlia <dindadahlia1211@gmail.com>
2023-12-20 21:38:47 +0700

    Package Model
    Membuat Package Model dan Isinya

---
+++ app/src/main/java/com/example/recipefood/model/ModelFilter.java
@@ -0,0 +1,34 @@
+package com.example.resepmakananv2.model;
+
+import java.io.Serializable;
+
+public class ModelFilter implements Serializable {
+
+    private String idMeal;
+    public String strMeal;
+    public String strMealThumb;
+
+    public String getIdMeal() {
+        return idMeal;
+    }
+
+    public void setIdMeal(String idMeal) {
+        this.idMeal = idMeal;
+    }
+
+    public String getStrMeal() {
+        return strMeal;
+    }
+
+    public void setStrMeal(String strMeal) {
+        this.strMeal = strMeal;
+    }
+
+    public String getStrMealThumb() {
+        return strMealThumb;
+    }
+
+    public void setStrMealThumb(String strMealThumb) {
+        this.strMealThumb = strMealThumb;
+    }
+}
